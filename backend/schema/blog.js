5e0c4d051731823b51d567fbb61796e9b6f3152f
kysakhin <akhinsreedalla1105@gmail.com>
2024-09-29 12:07:50 +0530

    29/09/24

--- backend/schema/blog.js
+++ backend/schema/blog.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
 const blog = new mongoose.Schema({
-  id: uuidv4(),
-  content: String,
-  date: String
+  id: {type: String, default: uuidv4 },
+  title: { type: String, required: true },
+  contentFile: {
+    type: String, 
+    required: true,
+    get: (filepath) => path.join(__dirname, "../uploads/", filepath)
+  },
+  category: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
-const Blogs = mongoose.model('blogs', blog);
+blog.set('toJSON', { getters: true })
 
-module.exports = Blogs;
+const Blog = mongoose.model('blog', blog);
+
+module.exports = Blog;
